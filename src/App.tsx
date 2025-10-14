1e1d29f31e8f8cabfe4b6c88dd718d765ba5d903
Tse Ay <114913072+tseayo@users.noreply.github.com>
2025-10-14 19:15:13 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -50,10 +50,11 @@ const App: React.FC = () => {
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
+            
+            {/* MOVE THESE INSIDE THE LAYOUT */}
+            <Chatbot />
+            <BackToTop />
           </Layout>
-
-          <Chatbot />
-          <BackToTop />
         </BrowserRouter>
       </TooltipProvider>
     </QueryClientProvider>
