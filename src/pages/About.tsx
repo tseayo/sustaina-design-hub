10fd99e216a0e0a03f22c1a2444e596a5b43a838
Tse Ay <114913072+tseayo@users.noreply.github.com>
2025-10-15 19:23:45 +0100

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,13 +1,11 @@
 import Navigation from "@/components/Navigation";
 import About from "@/components/About";
-import Footer from "@/components/Footer";
 
 const AboutPage = () => {
   return (
     <main className="min-h-screen">
       <Navigation />
       <About />
-      <Footer />
     </main>
   );
 };
