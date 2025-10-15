ebc87c3523e925d7cb5de081fd524ddf0ee50025
Tse Ay <114913072+tseayo@users.noreply.github.com>
2025-10-15 19:22:17 +0100

    Update Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,8 +1,10 @@
+import Navigation from "@/components/Navigation";
 import Services from "@/components/Services";
 
 const ServicesPage = () => {
   return (
     <main className="min-h-screen">
+      <Navigation />
       <Services />
     </main>
   );
