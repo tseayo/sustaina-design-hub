84ddbef781734a9594a545f30a4e22a7a2114c29
Tse Ay <114913072+tseayo@users.noreply.github.com>
2025-10-15 19:12:13 +0100

    Update Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,6 +1,4 @@
-import Navigation from "@/components/Navigation";
 import Services from "@/components/Services";
-import Footer from "@/components/Footer";
 
 const ServicesPage = () => {
   return (
