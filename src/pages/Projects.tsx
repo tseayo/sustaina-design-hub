015d399fe51c88783f93d62694f6dbad15983e2a
Tse Ay <114913072+tseayo@users.noreply.github.com>
2025-10-15 19:18:50 +0100

    Update Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,13 +1,11 @@
 import Navigation from "@/components/Navigation";
 import CaseStudies from "@/components/CaseStudies";
-import Footer from "@/components/Footer";
 
 const ProjectsPage = () => {
   return (
     <main className="min-h-screen">
       <Navigation />
       <CaseStudies />
-      <Footer />
     </main>
   );
 };
