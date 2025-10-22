dae99df1f2d942cc49974862525a58568f27818e
Tse Ay <114913072+tseayo@users.noreply.github.com>
2025-10-22 01:59:24 +0100

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,10 +11,12 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="min-h-screen flex flex-col">
       <Navigation />
-      <main className="flex-1 pt-16">  {/* Add pt-16 here */}
+      <main className="flex-1 pt-16">
         {children}
       </main>
       <Footer />
     </div>
   );
 };
+
+export default Layout;
